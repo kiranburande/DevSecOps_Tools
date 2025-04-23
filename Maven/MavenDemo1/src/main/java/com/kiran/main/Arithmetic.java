a636593d34a77ed29ef5f131b02d8cb6148fd256
Kiran Burande <kiransburande6172@gmail.com>
2025-04-23 16:19:19 +0530

    Add files via upload

---
+++ Maven/MavenDemo1/src/main/java/com/kiran/main/Arithmetic.java
@@ -0,0 +1,18 @@
+package com.kiran.main;
+
+public class Arithmetic 
+{
+	public int doSum(int a, int b)
+	{
+		return a+b;
+	}
+	public int doSub(int a, int b)
+	{
+		return a-b;
+	}
+	public int doMul(int a, int b)
+	{
+		return a*b;
+	}
+	
+}
---
+++ Maven/MavenDemo1/src/main/java/com/kiran/main/TestClassDemo.java
@@ -0,0 +1,10 @@
+package com.kiran.main;
+
+public class TestClassDemo
+{
+	public static void main(String[] args) {
+		Arithmetic a = new Arithmetic();
+		
+		System.out.println(a.doSum(10, 30));
+	}
+}
